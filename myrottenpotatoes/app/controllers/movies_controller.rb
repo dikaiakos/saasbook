22a829db6e08951205801791b3f09603c25ebe5a
dikaiakos <mdd@cs.ucy.ac.cy>
2014-06-17 16:55:13 +0300

    work out ch. 4.6, SaaS book

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -3,4 +3,27 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created"
+    redirect_to movies_path
+  end
+
+
+  private
+  def movie_params
+    params.require(:movie).permit! #(:title, :rating, :description, :release_date)
+  end
+
+
 end
